6f9e77d6d83ebed37d797acefe829efb1ea5855d
Shibaitle <shiba.despacito@gmail.com>
2026-04-12 15:40:23 +0700

    Add EMR models for NurseNote, RelativeNote, and WoundCareNote entities

---
+++ modules/entities/RelativeNote.go
@@ -0,0 +1,16 @@
+package entities
+
+import "time"
+
+type RelativeNote struct {
+	ID               string    `json:"relative_note_id" gorm:"primaryKey"`
+	ResidentID       string    `json:"resident_id" gorm:"not null"`
+	Relation         string    `json:"relation" gorm:"not null"`
+	Content          string    `json:"content" gorm:"not null"`
+	SendNote         bool      `json:"send_note" gorm:"not null;default:true"`
+	CreatedByStaffID string    `json:"created_by_staff_id" gorm:"type:text;not null"`
+	CreatedAt        time.Time `json:"created_at" gorm:"not null"`
+	UpdatedAt        time.Time `json:"updated_at" gorm:"not null"`
+
+	Resident Resident `json:"-" gorm:"foreignKey:ResidentID;references:ID"`
+}
