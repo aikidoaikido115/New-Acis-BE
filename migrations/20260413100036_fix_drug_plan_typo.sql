bdc1197f3882550f8ad2c8ecf61c58e18dad1ad1
Shibaitle <shiba.despacito@gmail.com>
2026-04-17 16:40:31 +0700

    remove migration for fixing typos in drug_plan column names

--- migrations/20260413100036_fix_drug_plan_typo.sql
+++
@@ -1,4 +0,0 @@
--- Rename a column from "is_ommitted" to "is_omitted"
-ALTER TABLE "drug_plans" RENAME COLUMN "is_ommitted" TO "is_omitted";
--- Rename a column from "ommitted_reason" to "omitted_reason"
-ALTER TABLE "drug_plans" RENAME COLUMN "ommitted_reason" TO "omitted_reason";
