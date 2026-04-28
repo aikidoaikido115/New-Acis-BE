55e4370627baedb72d18ff0d4de31b8738d042ca
SaRai <pasinlao2546@gmail.com>
2026-04-28 23:31:36 +0700

    migrate

---
+++ migrations/20260428090000_fix_support_ticket_reporter_role_constraint.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "support_tickets"
+  DROP CONSTRAINT IF EXISTS "chk_support_tickets_reporter_role";
+
+ALTER TABLE "support_tickets"
+  ADD CONSTRAINT "chk_support_tickets_reporter_role"
+  CHECK ("reporter_role" IN ('Medical Staff', 'Kitchen Staff', 'Relative', 'Super User', 'Admin'));
